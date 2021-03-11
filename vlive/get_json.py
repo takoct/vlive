749868e18d522eb106ac6b4dfd29a8cd748e4ffc
takoct <78149760+takoct@users.noreply.github.com>
2021-03-11 19:51:15 +0900

    repair

--- vlive/get_json.py
+++ vlive/get_json.py
@@ -1,9 +1,27 @@
 import urllib.request
-import const
+from const import HEADER
 import json
 
 def get_json(url):
-    req = urllib.request.Request(url,headers = const.HEADER)
+    req = urllib.request.Request(url,headers = HEADER)
     with urllib.request.urlopen(req) as res:
         res_json = json.loads(res.read())
     return res_json
+def get_json_from_vlive_html(req):
+    with urllib.request.urlopen(req) as res:
+        text = res.read().decode()
+        length = len(text)
+        index = text.find("window.__PRELOADED_STATE__")
+        text_json = ""
+        num_bracket_1 = 0
+        num_bracket_2 = 0
+        for i in range(index,length):
+            if text[i] == "{":
+                num_bracket_1 += 1
+            if text[i] == "}":
+                num_bracket_2 += 1
+            if num_bracket_1 > 0:
+                text_json += text[i]
+            if (num_bracket_1 == num_bracket_2) and (num_bracket_1 != 0):
+                break
+    return text_json
--- vlive/get_video_info.py
+++ vlive/get_video_info.py
@@ -1,49 +1,19 @@
 import urllib.request
 from progress import progress
-from get_json import get_json
+from get_json import get_json,get_json_from_vlive_html
 from const import APP_ID,HEADER
 import json
 
 def get_video_seq(url):
     req = urllib.request.Request(url,headers=HEADER)
-    with urllib.request.urlopen(req) as res:
-        text = res.read().decode()
-        length = len(text)
-        index = text.find("window.__PRELOADED_STATE__")
-        text_json = ""
-        num_bracket_1 = 0
-        num_bracket_2 = 0
-        for i in range(index,length):
-            if text[i] == "{":
-                num_bracket_1 += 1
-            if text[i] == "}":
-                num_bracket_2 += 1
-            if num_bracket_1 > 0:
-                text_json += text[i]
-            if (num_bracket_1 == num_bracket_2) and (num_bracket_1 != 0):
-                break
-        video_seq = json.loads(text_json)["postDetail"]["post"]["officialVideo"]["videoSeq"]
-        return video_seq
+    text_json = get_json_from_vlive_html(req)
+    video_seq = json.loads(text_json)["postDetail"]["post"]["officialVideo"]["videoSeq"]
+    return video_seq
 def get_vod_id(url):
     req = urllib.request.Request(url,headers=HEADER)
-    with urllib.request.urlopen(req) as res:
-        text = res.read().decode()
-        length = len(text)
-        index = text.find("window.__PRELOADED_STATE__")
-        text_json = ""
-        num_bracket_1 = 0
-        num_bracket_2 = 0
-        for i in range(index,length):
-            if text[i] == "{":
-                num_bracket_1 += 1
-            if text[i] == "}":
-                num_bracket_2 += 1
-            if num_bracket_1 > 0:
-                text_json += text[i]
-            if (num_bracket_1 == num_bracket_2) and (num_bracket_1 != 0):
-                break
-        vod_id = json.loads(text_json)["postDetail"]["post"]["officialVideo"]["vodId"]
-        return vod_id
+    text_json = get_json_from_vlive_html(req)
+    vod_id = json.loads(text_json)["postDetail"]["post"]["officialVideo"]["vodId"]
+    return vod_id
 def get_key(url):
     video_seq = get_video_seq(url)
     get_key_api_url = "https://www.vlive.tv/globalv-web/vam-web/video/v1.0/vod/{}/inkey?appId={}".format(video_seq,APP_ID)
@@ -56,5 +26,7 @@ def get_video_url(url):
     video_url = get_json(get_video_api_url)["videos"]["list"][0]["source"]
     return video_url
 def get_video_name(url):
-    return "test.mp4"
-    
+    req = urllib.request.Request(url,headers=HEADER)
+    text_json = get_json_from_vlive_html(req)
+    video_name = json.loads(text_json)["postDetail"]["post"]["officialVideo"]["title"]
+    return video_name 
