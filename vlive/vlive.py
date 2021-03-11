8a7a5d46f47537608fdc277a21c2ef91574a1fa1
takoct <78149760+takoct@users.noreply.github.com>
2021-03-11 21:59:29 +0900

    repair

--- vlive/vlive.py
+++ vlive/vlive.py
@@ -5,7 +5,7 @@ from download import download
 
 args = sys.argv
 url = sys.argv[1]
-if re.match("https://www.vlive.tv/post/.*",url) is None:
+if (re.match("https://www.vlive.tv/post/.*",url) is None) and (re.match("https://www.vlive.tv/video/.*",url) is None):
     exit()
 video_url = get_video_url(url)
 video_name = get_video_name(url)
